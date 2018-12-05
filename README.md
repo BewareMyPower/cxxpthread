b5e8de754a78df96345ca57aa6e95f8cb3c6a2bc
BewareMyPower <xyzinfernity@163.com>
2018-12-04 19:52:24 -0800

    complete test files

--- README.md
+++ README.md
@@ -4,5 +4,4 @@ A C++11 wrapper of pthread library, inspired by c++11 `<thread>`.
 It's a header-only library, and only requires compiler which supports C++11. (eg. gcc 4.8)
 However, if you want to enable macro `DEBUG`, Boost library is required.
 
-**TODO: It doesn't support function object yet.**
-see [function_object_test.cc](./test/function_object_test.cc)
+See examples in [test](./test).
--- test/function_object_test.cc
+++ test/function_object_test.cc
@@ -10,25 +10,41 @@ struct Object {
   void operator()() const { cout << "Object()" << endl; }
 };
 
+struct ObjectIntDouble {
+  void operator()(int i, double d) {  // non const
+    cout << "f(" << i << ", " << d << ")" << endl;
+  }
+};
+
 int main() {
-  // FIXME: compile error
-  //{
-  //  Object obj;
-  //  Thread t{obj};
-  //  t.join();
-  //}
-  cerr << "TODO: Thread(F&&, Args&&...) [F = Object&; Args={}]" << endl;
+  {
+    Object obj;
+    Thread t{obj};
+    t.join();
+  }
+  {
+    ObjectIntDouble obj;
+    Thread t{obj, 1, 3.14};
+    t.join();
+  }
+  // pass ref wrapper
+  {
+    Object obj;
+    auto const& crobj = obj;
+    Thread t{std::ref(crobj)};
+    t.join();
+  }
+  // compile error because ObjectIntDouble::operator() is non-const!
+//  {
+//    ObjectIntDouble obj;
+//    auto const& crobj = obj;
+//    Thread t{std::ref(crobj), 1, 3.14};
+//    t.join();
+//  }
   return 0;
 }
-/* error message:
-In file included from function_object_test.cc:2:0:
-../cxxpthread/thread.h: In instantiation of ‘cxxpthread::Thread::Thread(F&&, Args&& ...) [with F = Object&; Args = {}]’:
-function_object_test.cc:16:17:   required from here
-../cxxpthread/thread.h:32:72: error: cannot bind ‘Object’ lvalue to ‘Object&&’
-         new DerivedType(std::forward<F>(f), std::forward<Args>(args)...);
-                                                                        ^
-In file included from ../cxxpthread/thread.h:7:0,
-                 from function_object_test.cc:2:
-../cxxpthread/detail/thread_data.h:29:12: note:   initializing argument 1 of ‘cxxpthread::detail::ThreadData<F, Args>::ThreadData(F&&, Args&& ...) [with F = Object; Args = {}]’
-   explicit ThreadData(F&& f, Args&&... args)
+/*
+Object()
+f(1, 3.14)
+Object()
 */
