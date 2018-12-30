19dbb8ceb96eba2b5b43eea376f27a4f28b4152d
BewareMyPower <xyzinfernity@163.com>
2018-12-31 02:31:12 +0800

    Seperate some long method definitions

--- cxxpthread/thread.h
+++ cxxpthread/thread.h
@@ -2,6 +2,7 @@
 #ifndef CXXPTHREAD_THREAD_H
 #define CXXPTHREAD_THREAD_H
 
+#include <assert.h>
 #include <pthread.h>
 #include <memory>
 #include "detail/thread_data.h"
@@ -9,77 +10,82 @@
 
 namespace cxxpthread {
 
-class Thread {
+class Thread final {
  public:
   Thread() = default;
 
   Thread(const Thread&) = delete;
   Thread& operator=(const Thread&) = delete;
 
-  Thread(Thread&& rhs) noexcept : handle_(rhs.handle_), retval_(rhs.retval_) {
-    rhs.reset();
-  }
-
-  Thread& operator=(Thread&& rhs) noexcept {
-    if (handle_ == pthread_t{})
-      handleError("Thread move assign: can't use a unjoined Thread as lvalue");
-    handle_ = rhs.handle_;
-    retval_ = rhs.retval_;
-    rhs.reset();
-    return *this;
-  }
+  Thread(Thread&& rhs) noexcept;
+  Thread& operator=(Thread&& rhs) noexcept;
 
   // Start a thread function f(args...), if failed, print error and exit.
   template <typename F, typename... Args>
-  Thread(F&& f, Args&&... args) noexcept {
-    using BaseType = detail::BaseThreadData;
-    using DerivedType = detail::ThreadData<F, Args...>;
+  Thread(F&& f, Args&&... args) noexcept;
 
-    auto func = [](void* data) -> void* {
-      std::unique_ptr<BaseType> p(static_cast<BaseType*>(data));
-      p->run();
-      return nullptr;
-    };
+  // Join a thread, If failed, print error and exit.
+  void join() noexcept;
 
-    auto data =
-        new DerivedType(std::forward<F>(f), std::forward<Args>(args)...);
-    int error = pthread_create(&handle_, nullptr, func, data);
-    if (error != 0) handleError(error, "cxxpthread::Thread create failed");
+  // If thread function return a value, it innerlly call pthread_exit(), which
+  // parameter is a pointer to a memory allocated by new. To avoid memory leak,
+  // you must call getResult() to hold the pointer and automatically release it.
+  template <typename T>
+  std::unique_ptr<T> getResult() const {
+    return std::unique_ptr<T>(static_cast<T*>(ret_));
   }
 
-  void join() {
-    int error = pthread_join(handle_, &retval_);
-    if (error != 0) handleError(error, "cxxpthread::Thread::join()");
-    handle_ = pthread_t{};
-  }
+  // If thread function call pthread_join() to report an integral error code,
+  // getResult() will delete this error code and cause segment fault.
+  // To check the error code, you should call getExitCode().
+  long getExitCode() const noexcept { return reinterpret_cast<long>(ret_); }
 
-  // If thread function return a value, getResult() will return an unique_ptr
-  // object to that value.
-  // NOTE: User must specify correct ResultType, or it may cause fatal error.
-  template <typename ResultType>
-  std::unique_ptr<ResultType> getResult() const {
-    return std::unique_ptr<ResultType>(static_cast<ResultType*>(retval_));
+  static void exit(long error) {  // NOTE: noexcept will cause exception here
+    pthread_exit(reinterpret_cast<void*>(error));
   }
-
-  static void Exit(long retval) {
-    pthread_exit(reinterpret_cast<void*>(retval));
-  }
-
-  // If thread function use pthread_exit() to exit thread, getExitCode() will
-  // return exit code.
-  // If thread function return a value, getExitCode() will return a pointer to
-  // that value.
-  long getExitCode() const { return reinterpret_cast<long>(retval_); }
 
  private:
-  pthread_t handle_{};
-  void* retval_ = nullptr;
-
-  void reset() noexcept {
-    handle_ = pthread_t{};
-    retval_ = nullptr;
-  }
+  pthread_t handle_;
+  void* ret_ = nullptr;
 };
+
+// implement of Thread member
+
+Thread::Thread(Thread&& rhs) noexcept : handle_(rhs.handle_), ret_(rhs.ret_) {
+  rhs.handle_ = pthread_t{};
+  rhs.ret_ = nullptr;
+}
+
+Thread& Thread::operator=(Thread&& rhs) noexcept {
+  assert(this != &rhs);
+  handle_ = rhs.handle_;
+  ret_ = rhs.ret_;
+  rhs.handle_ = pthread_t{};
+  rhs.ret_ = nullptr;
+  return *this;
+}
+
+template <typename F, typename... Args>
+Thread::Thread(F&& f, Args&&... args) noexcept {
+  using BaseType = detail::BaseThreadData;
+  using DerivedType = detail::ThreadData<F, Args...>;
+
+  auto func = [](void* data) -> void* {
+    std::unique_ptr<BaseType> p(static_cast<BaseType*>(data));
+    p->run();
+    return nullptr;
+  };
+
+  auto data = new DerivedType(std::forward<F>(f), std::forward<Args>(args)...);
+  int error = pthread_create(&handle_, nullptr, func, data);
+  if (error != 0) handleError(error, "cxxpthread::Thread create failed");
+}
+
+void Thread::join() noexcept {
+  int error = pthread_join(handle_, &ret_);
+  if (error != 0) handleError(error, "cxxpthread::Thread::join()");
+  handle_ = pthread_t{};
+}
 
 }  // namespace cxxpthread
 
