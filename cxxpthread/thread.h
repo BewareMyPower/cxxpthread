168fdeb3f807f646ea64a54d4b9df129620349e7
BewareMyPower <xyzinfernity@163.com>
2019-01-07 12:41:44 +0800

    Add hasJoined() to get the state if thread has joined.

--- cxxpthread/thread.h
+++ cxxpthread/thread.h
@@ -24,25 +24,24 @@ class Thread final {
   template <typename F, typename... Args>
   Thread(F&& f, Args&&... args) noexcept;
 
+  // Unjoined thread will be joined in destructor.
+  ~Thread() noexcept;
+
   // Join a thread, If failed, print error and exit.
   void join() noexcept;
+
+  bool hasJoined() const noexcept { return has_joined_; }
 
   // If thread function return a value, it innerlly call pthread_exit(), which
   // parameter is a pointer to a memory allocated by new. To avoid memory leak,
   // you must call getResult() to hold the pointer and automatically release it.
   template <typename T>
-  std::unique_ptr<T> getResult() const {
-    return std::unique_ptr<T>(static_cast<T*>(ret_));
-  }
+  std::unique_ptr<T> getResult() const;
 
   // If thread function call pthread_join() to report an integral error code,
   // getResult() will delete this error code and cause segment fault.
   // To check the error code, you should call getExitCode().
-  long getExitCode() const noexcept {
-    static_assert(sizeof(long) == sizeof(void*),
-                  "getExitCode(): convert void* to long");
-    return reinterpret_cast<long>(ret_);
-  }
+  long getExitCode() const noexcept;
 
   // NOTE: noexcept will cause exception here because in C++, pthread_exit()
   // throws abi::__forced_unwind exception.
@@ -51,21 +50,26 @@ class Thread final {
  private:
   pthread_t handle_;
   void* ret_ = nullptr;
+  bool has_joined_ = true;
+
+  void init() noexcept;
 };
 
 // implement of Thread member
 
-Thread::Thread(Thread&& rhs) noexcept : handle_(rhs.handle_), ret_(rhs.ret_) {
-  rhs.handle_ = pthread_t{};
-  rhs.ret_ = nullptr;
+Thread::Thread(Thread&& rhs) noexcept : handle_(rhs.handle_),
+                                        ret_(rhs.ret_),
+                                        has_joined_(rhs.has_joined_) {
+  rhs.init();
 }
 
 Thread& Thread::operator=(Thread&& rhs) noexcept {
   assert(this != &rhs);
   handle_ = rhs.handle_;
   ret_ = rhs.ret_;
-  rhs.handle_ = pthread_t{};
-  rhs.ret_ = nullptr;
+  has_joined_ = rhs.has_joined_;
+
+  rhs.init();
   return *this;
 }
 
@@ -83,12 +87,45 @@ Thread::Thread(F&& f, Args&&... args) noexcept {
   auto data = new DerivedType(std::forward<F>(f), std::forward<Args>(args)...);
   int error = pthread_create(&handle_, nullptr, func, data);
   if (error != 0) handleError(error, "cxxpthread::Thread create failed");
+
+  has_joined_ = false;
 }
 
-void Thread::join() noexcept {
+Thread::~Thread() {
+  if (!hasJoined()) join();
+}
+
+inline void Thread::join() noexcept {
+  const char* where = "cxxpthread::Thread::join()";
+  if (hasJoined()) handleMessage("%s: Already has joined.", where);
+
   int error = pthread_join(handle_, &ret_);
-  if (error != 0) handleError(error, "cxxpthread::Thread::join()");
-  handle_ = pthread_t{};
+  if (error != 0) handleError(error, "%s: pthread_join failed", where);
+
+  has_joined_ = true;
+}
+
+template <typename T>
+inline std::unique_ptr<T> Thread::getResult() const {
+  const char* where = "cxxpthread::Thread::getResult<T>";
+  if (!hasJoined()) handleMessage("%s: Already has joined.", where);
+
+  return std::unique_ptr<T>(static_cast<T*>(ret_));
+}
+
+long Thread::getExitCode() const noexcept {
+  static_assert(sizeof(long) == sizeof(void*),
+                "getExitCode(): convert void* to long");
+  const char* where = "cxxpthread::Thread::getExitCode()";
+  if (!hasJoined()) handleMessage("%s: Already has joined.", where);
+
+  return reinterpret_cast<long>(ret_);
+}
+
+inline void Thread::init() noexcept {
+  handle_ = pthread_t();
+  ret_ = nullptr;
+  has_joined_ = true;
 }
 
 }  // namespace cxxpthread
